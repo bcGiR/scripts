e5d8343aa5b468167763f8d17cbe3e7ea6d188a5
Brendan Crawford <bcgir87@gmail.com>
2014-09-12 09:21:20 -0700

    added day change to stock display

--- stocks.sh
+++ stocks.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 #Script for displaying my portfolio
 
-echo "Emera Inc.: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AEMA&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}')"
-echo "The Bank of Nova Scotia: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3ABNS&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}')"
-echo "Bank of Montreal: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3ABMO&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}')"
-echo "TransCanada Corporation: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3ATRP&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}')"
-echo "TELUS Corporation: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AT&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}')"
-echo "Enbridge Inc.: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AENB&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}')"
-echo "RioCan REIT: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AREI.UN&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}')"
+#Display Emera Inc. Price and Day Change
+echo "Emera Inc.: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AEMA&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}') $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AEMA&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span class="ch bld"' | awk -F ">" '{print $3}' | awk -F "<" '{print $1}')"
+
+#Display Bank of Nova Scotia Price and Day Change
+echo "The Bank of Nova Scotia: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3ABNS&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}') $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3ABNS&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span class="ch bld"' | awk -F ">" '{print $3}' | awk -F "<" '{print $1}')"
+
+#Display Bank of Montreal Price and Day Change
+echo "Bank of Montreal: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3ABMO&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}') $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3ABMO&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span class="ch bld"' | awk -F ">" '{print $3}' | awk -F "<" '{print $1}')"
+
+#Display TransCanada Corporation Price and Day Change
+echo "TransCanada Corporation: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3ATRP&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}') $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3ATRP&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span class="ch bld"' | awk -F ">" '{print $3}' | awk -F "<" '{print $1}')"
+
+#Display TELUS Corporation Price and Day Change
+echo "TELUS Corporation: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AT&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}') $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AT&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span class="ch bld"' | awk -F ">" '{print $3}' | awk -F "<" '{print $1}')"
+
+#Display Enbridge Inc. Price and Day Change
+echo "Enbridge Inc.: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AENB&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}') $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AENB&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span class="ch bld"' | awk -F ">" '{print $3}' | awk -F "<" '{print $1}')"
+
+#Display RioCan REIT Price and Day Change
+echo "RioCan REIT: $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AREI.UN&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span id="ref_' | awk -F ">" '{print $2}' | awk -F "<" '{print $1}') $(curl --silent -X Get "https://www.google.ca/finance?q=TSE%3AREI.UN&ei=4OQRVMHAKemziALijIDoDQ" > /tmp/ChangeIsInevitable && cat /tmp/ChangeIsInevitable | grep -m1 -E 'span class="ch bld"' | awk -F ">" '{print $3}' | awk -F "<" '{print $1}')"
