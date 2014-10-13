3d8372ce9cb10153c68768fa83f807cb9f62aee5
Brendan Crawford <bcgir87@gmail.com>
2014-10-13 11:45:19 -0700

    bjupdown now emails from script instead of crontab

--- bjupdown.sh
+++ bjupdown.sh
@@ -5,5 +5,5 @@ echo -e "GET / HTTP/1.0\n" >&3
 RESPONSE=$( cat <&3 | head -1 | grep -o '[0-9]\{3\}')
 if [ !$RESPONSE == 200 ]
 then
-    echo "brendanjames.ca appears to be down"
+    echo "brendanjames.ca appears to be down" | mail -s "brendanjames.ca error" bcgir87@gmail.com
 fi
